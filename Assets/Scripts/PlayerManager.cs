cc8ccbf9dc68b25f815665dcb00dc5e1a2134c2d
DevEoin <116293113+DevEoin@users.noreply.github.com>
2024-02-27 23:40:57 +0000

    Implemented button functionality for power-ups and added comments
    Power-ups can now only activate when hasPowerUp and the power-up's boolean are true. All changes are limited to the Multiplayer scene (prefabs remain unchanged). Comments added to improve readability

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -15,11 +15,12 @@ public class PlayerManager : MonoBehaviour
     
     [SerializeField] private SkinnedMeshRenderer bodyMeshRenderer;
     [SerializeField] private Collider playerCollider;
-    
 
-    private int powers = 3;
-
-    private bool hasPowerUp = false;
+    // Power-Up variables
+    private int powers = 3;             // represents the number of power-ups currently in the game
+    private int powerSelect = 0;            // represents the power-up to be chosen at runtime 
+    private bool hasPowerUp = false;        // denotes whether a player has a power-up ability
+    private float powerUpTimer = 10f;       // indicates the length of time between the expiry of one power-up and the acquisition of another
 
     public String name;
 
@@ -42,57 +43,69 @@ public class PlayerManager : MonoBehaviour
         rb = GetComponent<Rigidbody>();
         playerMovement = GetComponent<PlayerMovement>();
         playerInput = GetComponent<PlayerInput>(); ;
-        
-
+       
         name = gameObject.name;
 
-        //InvokeRepeating("EnablePowerUp", 5f, 20f);
+        hasPowerUp = true;
     }
+
 
     // Update is called once per frame
     void Update(){
-        hasPowerUp = true;
+        if (!hasPowerUp) { 
+            StartCoroutine(PowerUpTimer());
+        }
+        if (hasPowerUp)
+        {
+            powerSelect = EnablePowerUp();
+        }
     }
+
 
     private void OnTriggerEnter(Collider other){
 
     }
 
+
     private void OnCollisionEnter(Collision collision){
         
     }
 
+
+    /*
+     * Waits for a number of seconds equal to powerUpTimer before calling the method EnablePowerUp(). To be called when an instance's hasPowerUp is set to false by any means. 
+     */
+    private IEnumerator PowerUpTimer()
+    {
+        yield return new WaitForSeconds(powerUpTimer);
+        hasPowerUp = true;
+    }
+
+
+    /*
+     * Provides functionality to the PowerUp action within the playerInput action map. On activation, starts the coroutine of the relevant power-up based on the chosen power up selected by 
+     * EnablePowerUp() utilising the integer, powerSelect, as the means to select a coroutine.
+     */
     private void OnPowerUp()
     {
-        Debug.Log("Button pressed");
+        Debug.Log("Power-Up button pressed; hasPowerUp = " + hasPowerUp);
         if (hasPowerUp)
-        {
-            int powerSelect = Mathf.RoundToInt(UnityEngine.Random.Range(1, powers));
-            
+        {  
             switch (powerSelect)
             {
-                case 1:
-                    
-                    
+                case 1:                 
                         speedBoosting = true;
-                        StartCoroutine(SpeedBoosting());
-                    
+                        StartCoroutine(SpeedBoosting());       
                     break;
 
-                case 2:
-                  
-                    
+                case 2:                                     
                         isGhosting = true;
-                        StartCoroutine( Ghosting());
-                    
+                        StartCoroutine(Ghosting());                    
                     break;
 
-                case 3:
-                    
-                    
+                case 3:                                     
                         isInvisible = true;
-                        StartCoroutine(SelfInvisibility());
-                    
+                        StartCoroutine(SelfInvisibility());                  
                     break;
 
                 default:
@@ -102,45 +115,44 @@ public class PlayerManager : MonoBehaviour
         }
     }
 
-    
-    private void EnablePowerUp(){
+
+    /*
+     * Returns a random integer value representing the power to be selected. Sets the corresponding power-up boolean to true to enable to corresponding power-up methods.
+     */
+    private int EnablePowerUp(){
         
-        int powerSelect = Mathf.RoundToInt(UnityEngine.Random.Range(1, powers));
+        powerSelect = Mathf.RoundToInt(UnityEngine.Random.Range(1, powers+1));
         
         switch (powerSelect)
         {
-            case 1:
-                if (hasPowerUp == false)
-                {
+            case 1:               
                     speedBoosting = true;
-                    //SpeedBoosting();
-                }
-                break;
+                    return 1;
+                //break;
 
-            case 2:
-                if (hasPowerUp == false)
-                {
+            case 2:              
                     isGhosting = true;
-                    //Ghosting();
-                }
-                break;
+                    return 2;
+                
+                //break;
 
-            case 3:
-                if (hasPowerUp == false)
-                {
+            case 3:                
                     isInvisible = true;
-                    //SelfInvisibility();
-                }
-                break;
+                    return 3;                
+               // break;
 
             default:
-                Debug.Log("powerSelect drew a value not present in the powers array");
+                Debug.Log("Death at powerSelect. See EnablePowerUp() :(");
                 break;
         }
-        
+        hasPowerUp = true;
+        return powerSelect;
     }
     
-    
+    /*
+     * Doubles the speed of the instance of the object that has this script attached to it. Once the speed is increased and subsequently decreased after the speedBoostDuration, then 
+     * the speedBoosting and hasPowerUp booleans are set to false, enabling the player to make use of power-ups once again. 
+     */
     private IEnumerator SpeedBoosting(){
 
         if (gameObject.name == "Blue Player")
@@ -163,26 +175,51 @@ public class PlayerManager : MonoBehaviour
             speedBoosting = false;
             hasPowerUp = false;
         }
+
+        if (gameObject.name == "Yellow Player")
+        {
+            player3Movement.speed *= 2;
+            yield return new WaitForSeconds(speedBoostDuration);
+            player3Movement.speed /= 2;
+
+            speedBoosting = false;
+            hasPowerUp = false;
+        }
     }
     
-
+    /*
+     * Ignores collisions of the player and all instances of the maze wall prefab for the length of ghostingDuration. Afterwards the isGhosting and hasPowerUp values are set to false so 
+     * as to enable power-up selection once more.
+     */
     private IEnumerator Ghosting(){
-        
-        foreach (GameObject ga in walls){
-            Physics.IgnoreCollision(playerCollider, ga.GetComponent<Collider>(), true);
+
+        if (isGhosting)
+        {
+            foreach (GameObject ga in walls)
+            {
+                Physics.IgnoreCollision(playerCollider, ga.GetComponent<Collider>(), true);
+            }
         }
-        
 
         Debug.Log("Ghosting");
 
         yield return new WaitForSeconds(ghostingDuration);
         isGhosting = false;
-        hasPowerUp=false;
+        foreach (GameObject ga in walls)
+        {
+            Physics.IgnoreCollision(playerCollider, ga.GetComponent<Collider>(), false);
+        }
+        hasPowerUp = false;
        
     }
 
+
+    /*
+     * Disables the Mesh Renderer inserted into the SerializedField at the beginning of the class. After execution isInvisible and hasPowerUp are disabled to enable other methods to function.
+     */
     private IEnumerator SelfInvisibility()
     {
+
         bodyMeshRenderer.enabled = false;
         yield return new WaitForSeconds(invisDuration);
         bodyMeshRenderer.enabled = true;
