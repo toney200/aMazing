0899a6021837329c4aaa824231cdbb1e4a6fcde0
Dimitrije Sreckovic <dimitrijesreckovic02@gmail.com>
2024-02-28 15:53:46 +0000

    Maze restarts when a player enters the goal

--- Assets/Scripts/MazeGoal.cs
+++ Assets/Scripts/MazeGoal.cs
@@ -6,11 +6,15 @@ public class MazeGoal : MonoBehaviour
 {
     private void OnTriggerEnter(Collider other)
     {
-        
+        if(other.GetComponent<Collider>().tag == "Player")
+        {
+            RoundFinished();
+        }
     }
 
     private void RoundFinished()
     {
-
+        Transform parent = gameObject.transform.parent;
+        parent.GetComponent<MazeGeneratorInstant>().NewRound();
     }
 }
